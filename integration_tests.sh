f62eb2767fd279a579129a71e14460d39a1d0c42
Eduardo Ferro Aldama <eduardo.ferro.aldama@gmail.com>
2015-09-23 18:57:25 +0200

    Updated snmpsimd command line

--- integration_tests.sh
+++ integration_tests.sh
@@ -7,7 +7,7 @@ DEVELOP_ENV=$SCRIPT_DIR/env_develop
 find . -name *pyc* -delete
 trap "pkill -f snmpsimd.py; exit" SIGHUP SIGINT SIGTERM
 
-${VIRTUAL_ENV}/bin/python ${VIRTUAL_ENV}/bin/snmpsimd.py --v2c-arch --agent-port=1161 --device-dir=integration_tests/snmpsim/simulated_data/ --validate-device-data &
+${VIRTUAL_ENV}/bin/python ${VIRTUAL_ENV}/bin/snmpsimd.py --v2c-arch --agent-udpv4-endpoint=127.0.0.1:1161 --device-dir=integration_tests/snmpsim/simulated_data/ --validate-device-data &
 
 nosetests $INTEGRATION_TESTS -s --logging-clear-handlers --processes=16 --process-timeout=20
 RETCODE=$?
