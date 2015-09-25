42942affab263a9704995d0ced2118096e3eace3
Eduardo Ferro Aldama <eduardo.ferro.aldama@gmail.com>
2015-09-25 15:46:25 +0200

    Intial steps to use a dockerized snmpd server for the tests

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM alpine
+MAINTAINER eduardo.ferro.aldama@gmail.com
+
+RUN apk add --update net-snmp bash && rm -rf /var/cache/apk/*
+COPY snmpd.conf /etc/snmp/snmpd.conf
+
+EXPOSE 161/udp
+ENTRYPOINT ["/usr/sbin/snmpd"]
--- integration_tests.sh
+++ integration_tests.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 #set -e
 
-SCRIPT_DIR=`dirname $0`
-DEVELOP_ENV=$SCRIPT_DIR/env_develop
-
-find . -name *pyc* -delete
-trap "pkill -f snmpsimd.py; exit" SIGHUP SIGINT SIGTERM
-
-${VIRTUAL_ENV}/bin/python ${VIRTUAL_ENV}/bin/snmpsimd.py --v2c-arch --agent-udpv4-endpoint=127.0.0.1:1161 --device-dir=integration_tests/snmpsim/simulated_data/ --validate-device-data &
-
-nosetests $INTEGRATION_TESTS -s --logging-clear-handlers --processes=16 --process-timeout=20
-RETCODE=$?
-
-pkill -f snmpsimd.py
-sleep 1
-exit $RETCODE
-
-find . -name *pyc* -delete
+nosetests integration_tests -s --logging-clear-handlers --processes=16 --process-timeout=20
