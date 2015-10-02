3bcb9de841cc26e11758bdb0b7b99e3f0a29369c
Eduardo Ferro Aldama <eduardo.ferro.aldama@gmail.com>
2015-10-02 16:57:14 +0200

    Fixe localtime at the snmpd docker

--- integration_tests.sh
+++ integration_tests.sh
@@ -2,7 +2,7 @@
 #set -e
 
 docker build -t snmpd .
-docker run --rm -p 1161:161/udp --name snmpd -t snmpd &
+docker run --rm -v /etc/localtime:/etc/localtime:ro -p 1161:161/udp --name snmpd -t snmpd &
 nosetests integration_tests -s --logging-clear-handlers --processes=16 --process-timeout=20
 docker stop snmpd
 
