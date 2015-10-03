ade087e58bc32e8961665b9116262357824243df
Eduardo Ferro Aldama <eduardo.ferro.aldama@gmail.com>
2015-10-03 11:14:07 +0200

    travis generate an error stoping the docker

--- integration_tests.sh
+++ integration_tests.sh
@@ -4,7 +4,8 @@
 docker build -t snmpd .
 docker run --rm -v /etc/localtime:/etc/localtime:ro -p 1161:161/udp --name snmpd -t snmpd &
 nosetests integration_tests -s --logging-clear-handlers --processes=16 --process-timeout=20
-docker stop snmpd
+docker stop snmpd || echo
+
 
 
 
