c896419f437799d30f338770e24b299d2667c856
Isidro Lopez <ijlopez@alea-soluciones.com>
2017-03-13 16:35:59 +0100

    Fix integration_tests.sh: return code should be the test execution return code

--- integration_tests.sh
+++ integration_tests.sh
@@ -4,8 +4,6 @@
 docker build -t snmpd .
 docker run --rm -v /etc/localtime:/etc/localtime:ro -p 1161:161/udp --name snmpd -t snmpd &
 nosetests integration_tests -s --logging-clear-handlers --processes=16 --process-timeout=20
+NOSE_RETCODE=$?
 docker stop snmpd || echo
-
-
-
-
+exit $NOSE_RETCODE
