2f799dc79def2c8bb22bd26c72a51c56de384e83
Isidro López <islomar@gmail.com>
2017-03-15 16:59:16 +0100

    [tests] Sleep 10 seconds before executing tests

--- integration_tests.sh
+++ integration_tests.sh
@@ -4,8 +4,8 @@ set -e
 trap "pkill -f snmpsimd.py; exit" SIGHUP SIGINT SIGTERM ERR EXIT
 python $(which snmpsimd.py) --v2c-arch --agent-port=1161 --device-dir=integration_tests/snmpsim/simulated_data/ --validate-device-data 2>/dev/null 1>/dev/null &
 ps -fea | grep snmpsim
+sleep 10
 echo "Checking snmsip port up and running"
-sleep 1
 ss -unl
 nosetests $INTEGRATION_TESTS -s --logging-clear-handlers --processes=16 --process-timeout=50 --with-yanc
 NOSE_RETCODE=$?
