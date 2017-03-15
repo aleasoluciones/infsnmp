9a282cda95cf0f582bb61918a98c57702bc63057
Isidro López <islomar@gmail.com>
2017-03-15 15:45:19 +0100

    Move test philosophy from inffelix/snmp: use snmpsim instead of snmpd Docker

--- integration_tests.sh
+++ integration_tests.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-#set -e
+set -e
 
-docker build -t snmpd .
-docker run --rm -v /etc/localtime:/etc/localtime:ro -p 1161:161/udp --name snmpd -t snmpd &
-nosetests integration_tests -s --logging-clear-handlers --processes=16 --process-timeout=20
+trap "pkill -f snmpsimd.py; exit" SIGHUP SIGINT SIGTERM
+python $(which snmpsimd.py) --v2c-arch --agent-port=1161 --device-dir=integration_tests/snmpsim/simulated_data/ --validate-device-data 2>/dev/null 1>/dev/null &
+nosetests $INTEGRATION_TESTS -s --logging-clear-handlers --processes=16 --process-timeout=50 --with-yanc
 NOSE_RETCODE=$?
-docker stop snmpd || echo
 exit $NOSE_RETCODE
--- integration_tests/test_snmp_client.py
+++ integration_tests/test_snmp_client.py
@@ -23,44 +23,47 @@ class SnmpClientTest(unittest.TestCase):
 
     def setUp(self):
         self.snmp_client = clients.PySnmpClient()
-        self.community = 'public'
+        self.community = 'c4-temperatures'
         self.host = '127.0.0.1'
         self.port = 1161
 
     def test_get_several_oids(self):
-        oid1 = '1.3.6.1.2.1.1.4.0'
-        oid2 = '1.3.6.1.2.1.1.6.0'
+        oid1 = '1.3.6.1.4.1.4998.1.1.10.1.4.2.1.29.1.10'
+        oid2 = '1.3.6.1.4.1.4998.1.1.10.1.4.2.1.29.1.11'
 
         result = self.snmp_client.get(
             self.host, self.community, [oid1, oid2], port=self.port)
 
         assert_that(
-            result, equal_to([(oid1, snmp_string("infsnmptests <infsnmptests@example.org>")),
-                              (oid2, snmp_string("infsnmptests"))]))
+            result, equal_to([(oid1, snmp_integer(999)), (oid2, snmp_integer(31))]))
 
     def test_walk(self):
-        oid = '1.3.6.1.2.1.1.9.1.3'
-        oid1 = '1.3.6.1.2.1.1.9.1.3.4'
+        oid = '1.3.6.1.4.1.4998.1.1.10.1.4.2.1.29'
+        oid1 = '1.3.6.1.4.1.4998.1.1.10.1.4.2.1.29.1.10'
+        oid2 = '1.3.6.1.4.1.4998.1.1.10.1.4.2.1.29.1.11'
 
         result = self.snmp_client.walk(
             self.host, self.community, oid, port=self.port)
 
-        assert_that(result, has_length(10))
-        assert_that(result, has_item((oid1, snmp_string("The MIB module for SNMPv2 entities"))))
+        assert_that(result, has_length(31))
+        assert_that(result, has_item((oid1, snmp_integer(999))))
+        assert_that(result, has_item((oid2, snmp_integer(31))))
 
     def test_bulk_walk(self):
-        oid = '1.3.6.1.2.1.1.9.1.3'
-        oid1 = '1.3.6.1.2.1.1.9.1.3.4'
+        oid = '1.3.6.1.4.1.4998.1.1.10.1.4.2.1.29'
+        oid1 = '1.3.6.1.4.1.4998.1.1.10.1.4.2.1.29.1.10'
+        oid2 = '1.3.6.1.4.1.4998.1.1.10.1.4.2.1.29.1.11'
 
         result = self.snmp_client.bulk_walk(
             self.host, self.community, oid, port=self.port)
 
-        assert_that(result, has_length(10))
-        assert_that(result, has_item((oid1, snmp_string("The MIB module for SNMPv2 entities"))))
+        assert_that(result, has_length(31))
+        assert_that(result, has_item((oid1, snmp_integer(999))))
+        assert_that(result, has_item((oid2, snmp_integer(31))))
 
     def test_get_with_timeout(self):
         self.assertRaises(exceptions.SNMPExceptionError, self.snmp_client.get,
-                          '127.0.0.1', 'incorrect-community', ['1.1'], port=self.port, timeout=1)
+                          '127.0.0.1', 'incorrect-community', ['1.1'], port=self.port)
 
     def test_get_unknown_oid(self):
         unknown_oid = '1.2'
@@ -80,22 +83,23 @@ class SnmpClientTest(unittest.TestCase):
         assert_that(result, has_length(0))
 
     def test_set(self):
-        oid_1 = '1.3.6.1.2.1.1.5.0'
-        value_1 = snmp_octect_string('irrelevant_value1').value()
-        snmp_set_values = ((oid_1, value_1), )
-        community = 'rwcommunity'
+        oid_1 = '1.3.6.1.2.1.1.1.0'
+        oid_2 = '1.3.6.1.2.1.1.3.0'
+        value_1 = snmp_octect_string('irrelevant_value').value()
+        value_2 = snmp_integer(22).value()
+        snmp_set_values = ((oid_1, value_1), (oid_2, value_2),)
+        community = 'set'
 
         self.snmp_client.set(
-            self.host, community, snmp_set_values, port=self.port, timeout=1)
+            self.host, community, snmp_set_values, port=self.port)
 
         result = self.snmp_client.get(
-            self.host, community, [oid_1], port=self.port, timeout=1)
-
+            self.host, community, [oid_1, oid_2], port=self.port)
         assert_that(
-            result, equal_to([(oid_1, snmp_string("irrelevant_value1"))]))
+            result, equal_to([(oid_1, types.PySnmpValue(value_1)), (oid_2, types.PySnmpValue(value_2))]))
 
     def test_set_with_timeout(self):
-        oid_1 = '1.3.6.1.2.1.1.5.0'
+        oid_1 = '1.3.6.1.2.1.1.1.0'
         value_1 = rfc1902.OctetString('hola')
 
         self.assertRaises(exceptions.SNMPExceptionError, self.snmp_client.set,
