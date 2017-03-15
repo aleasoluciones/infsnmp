1829b6dd1c83709b0ae3648008f8bb152a119cb1
Isidro López <islomar@gmail.com>
2017-03-15 16:24:14 +0100

    Include raven dependency and kill snmpsimd when error or exit signals

--- integration_tests.sh
+++ integration_tests.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
-trap "pkill -f snmpsimd.py; exit" SIGHUP SIGINT SIGTERM
+trap "pkill -f snmpsimd.py; exit" SIGHUP SIGINT SIGTERM ERR EXIT
 python $(which snmpsimd.py) --v2c-arch --agent-port=1161 --device-dir=integration_tests/snmpsim/simulated_data/ --validate-device-data 2>/dev/null 1>/dev/null &
+ps -fea | grep snmpsim
 nosetests $INTEGRATION_TESTS -s --logging-clear-handlers --processes=16 --process-timeout=50 --with-yanc
 NOSE_RETCODE=$?
 exit $NOSE_RETCODE
