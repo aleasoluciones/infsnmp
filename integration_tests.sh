9476cd63bb66452ca942d79800559c395038ea78
Isidro López <islomar@gmail.com>
2017-03-15 17:02:54 +0100

    [tests] Show snmpsimd execution

--- integration_tests.sh
+++ integration_tests.sh
@@ -2,7 +2,7 @@
 set -e
 
 trap "pkill -f snmpsimd.py; exit" SIGHUP SIGINT SIGTERM ERR EXIT
-python $(which snmpsimd.py) --v2c-arch --agent-port=1161 --device-dir=integration_tests/snmpsim/simulated_data/ --validate-device-data 2>/dev/null 1>/dev/null &
+python $(which snmpsimd.py) --v2c-arch --agent-port=1161 --device-dir=integration_tests/snmpsim/simulated_data/ --validate-device-data
 ps -fea | grep snmpsim
 sleep 10
 echo "Checking snmsip port up and running"
