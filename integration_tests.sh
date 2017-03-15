b1286751dfe7b69205d00525eb9100a2552d9002
Isidro López <islomar@gmail.com>
2017-03-15 16:40:29 +0100

    [tests] Remove -p from netstat (fails in Travis)

--- integration_tests.sh
+++ integration_tests.sh
@@ -4,7 +4,7 @@ set -e
 trap "pkill -f snmpsimd.py; exit" SIGHUP SIGINT SIGTERM ERR EXIT
 python $(which snmpsimd.py) --v2c-arch --agent-port=1161 --device-dir=integration_tests/snmpsim/simulated_data/ --validate-device-data 2>/dev/null 1>/dev/null &
 ps -fea | grep snmpsim
-netstat -putan | grep 1161
+netstat -unta | grep 1161
 nosetests $INTEGRATION_TESTS -s --logging-clear-handlers --processes=16 --process-timeout=50 --with-yanc
 NOSE_RETCODE=$?
 exit $NOSE_RETCODE
