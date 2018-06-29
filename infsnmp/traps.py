24409cb98e15ce9fe1db3f9ab39d1336481200ff
Jorge Senín <jsenin@alea-soluciones.com>
2018-06-29 10:25:51 +0200

    Undo split logs to critical log, moved remote address to the begining

--- infsnmp/traps.py
+++ infsnmp/traps.py
@@ -80,8 +80,7 @@ class PySnmpTrapDispatcher(object):
                             values=values)
                 )
             except TypeError:
-                logger.critical('Error processing RequestPDU transport_address: {}. Please enable log debug to view request_pdu'.format(transport_address), exc_info=True)
-                logger.debug('Error processing RequestPDU transport_address {} request_pdu:{}'.format(transport_address, request_pdu), exc_info=True)
+                logger.critical('Error processing RequestPDU transport_address:{} request_pdu:{}'.format(transport_address, request_pdu), exc_info=True)
 
 
     def _extract_value(self, val):
