0b3da9207aa217a81d2ed164fecf76dcc6be3a19
Jorge Senín <jsenin@alea-soluciones.com>
2019-05-29 14:37:50 +0200

    [INTERNAL] Fix missing depedency pyasn==0.1.2 and add regression and unit tests

--- dev/integration_tests.sh
+++ dev/integration_tests.sh
@@ -5,7 +5,7 @@ trap "docker stop -t 0 snmpsimd; docker rm snmpsimd; exit" SIGHUP SIGINT SIGTERM
 docker build -t snmpsimd .
 docker run -d --name snmpsimd -v /etc/localtime:/etc/localtime:ro -v $(pwd)/integration_tests/snmpsim/simulated_data/:/simulated_data -p 1161:1161/udp snmpsimd
 sleep 5
-nosetests $INTEGRATION_TESTS -s --logging-clear-handlers --processes=16 --process-timeout=50 --with-yanc
+nosetests integration_tests/ -s --logging-clear-handlers --processes=16 --process-timeout=50 --with-yanc
 NOSE_RETCODE=$?
 docker stop -t 0 snmpsimd
 docker rm snmpsimd
--- specs/snmp_trap_dispatcher_spec.py
+++ specs/snmp_trap_dispatcher_spec.py
@@ -1,57 +1,34 @@
-from mamba import describe, context, it
+from mamba import describe, context, it, before
 from expects import expect, equal
 from doublex import Spy
 
 from infsnmp.traps import PySnmpTrapDispatcher
-from pysnmp.proto import api
-
-
-PROTO_MODULE = api.protoModules[api.protoVersion2c]
-
+from specs import helpers
 
 with describe('PySnmpTrapDispatcher Spec'):
     with context('FEATURE: is snmp trap OID'):
+        with before.each:
+            trap_hander = Spy()
+            address = Spy()
+            port = Spy()
+            clock = Spy()
+            self.pysnmp_trap_dispatcher = PySnmpTrapDispatcher(trap_hander, address, port, clock)
+
         with context('having a trap oid'):
             with it('returs true'):
                 an_str_oid = '1.3.6.1.6.3.1.1.4.1.0'
-                request_pdu = _build_request_pdu(an_str_oid)
-                an_oid = _get_oid_from_request_pdu(request_pdu)
+                an_oid = helpers.build_oid_object_from(an_str_oid)
 
-                trap_hander = Spy()
-                address = Spy()
-                port = Spy()
-                clock = Spy()
-                pysnmp_trap_dispatcher = PySnmpTrapDispatcher(trap_hander, address, port, clock)
-
-                is_snmp_trap_oid = pysnmp_trap_dispatcher.is_snmp_trap_oid(an_oid)
+                is_snmp_trap_oid = self.pysnmp_trap_dispatcher.is_snmp_trap_oid(an_oid)
 
                 expect(is_snmp_trap_oid).to(equal(True))
 
         with context('having a NON trap oid'):
             with it('returs false'):
                 an_str_oid = '1.2.3.4.5.6.7'
-                request_pdu = _build_request_pdu(an_str_oid)
-                an_oid = _get_oid_from_request_pdu(request_pdu)
+                an_oid = helpers.build_oid_object_from(an_str_oid)
 
-                trap_hander = Spy()
-                address = Spy()
-                port = Spy()
-                clock = Spy()
-                pysnmp_trap_dispatcher = PySnmpTrapDispatcher(trap_hander, address, port, clock)
-
-                is_snmp_trap_oid = pysnmp_trap_dispatcher.is_snmp_trap_oid(an_oid)
+                is_snmp_trap_oid = self.pysnmp_trap_dispatcher.is_snmp_trap_oid(an_oid)
 
                 expect(is_snmp_trap_oid).to(equal(False))
 
-def _build_request_pdu(str_oid):
-    request_pdu = PROTO_MODULE.GetRequestPDU()
-    PROTO_MODULE.apiPDU.setDefaults(request_pdu)
-    PROTO_MODULE.apiPDU.setVarBinds(
-        request_pdu, ((str_oid, PROTO_MODULE.Null('')),
-                        )
-    )
-    return request_pdu
-
-def _get_oid_from_request_pdu(request_pdu):
-    for oid, val in PROTO_MODULE.apiPDU.getVarBindList(request_pdu):
-        return oid
